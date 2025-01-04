55e1067088d450ba3fb621e46e1eeba74c3165e8
Lakshmipriya531 <priyapasupuleti561@gmail.com>
2025-01-04 12:00:32 +0530

    Add files via upload

---
+++ Demo2.java
@@ -0,0 +1,13 @@
+public class Demo2 {
+    public static void main(String[] args) {
+        int num1=30;
+        int num2=40;
+        if(num1>num2){
+            System.out.println("num1 is less than num2");
+        }
+        else{
+            System.out.println("num1 is are greater than num2");
+        }
+    } 
+    
+}
