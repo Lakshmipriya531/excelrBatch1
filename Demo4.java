5793b966e74d0383e0c7dafc3e7dc304e65e49c5
Lakshmipriya531 <priyapasupuleti561@gmail.com>
2025-01-04 12:33:14 +0530

    Add files via upload

---
+++ Demo4.java
@@ -0,0 +1,10 @@
+public class Demo4 {
+    public static void main(String[] args){
+        int i=1;
+        for(i=1;i<=100;i++){
+            System.out.println(i);
+        }
+        }
+    }
+    
+
