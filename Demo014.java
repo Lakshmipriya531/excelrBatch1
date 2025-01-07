0f057b970b1661e1dde8f45cfe01ad653a61f3cb
Lakshmipriya531 <priyapasupuleti561@gmail.com>
2025-01-07 12:43:57 +0530

    Add files via upload

---
+++ Demo014.java
@@ -0,0 +1,33 @@
+public class Demo014 {
+        public static void main(String[] args) {
+            int num = 5;
+    
+            
+            System.out.println("Unary plus: " + (+num)); // Output: 5
+    
+            
+            System.out.println("Unary minus: " + (-num)); // Output: -5
+    
+            
+            System.out.println("Prefix increment: " + (++num)); // Output: 6
+    
+            
+            System.out.println("Postfix increment: " + (num++)); // Output: 6
+            System.out.println("After postfix increment: " + num); // Output: 7
+    
+            
+            System.out.println("Prefix decrement: " + (--num)); // Output: 6
+    
+            
+            System.out.println("Postfix decrement: " + (num--)); // Output: 6
+            System.out.println("After postfix decrement: " + num); // Output: 5
+    
+            
+            boolean flag = true;
+            System.out.println("Logical NOT: " + (!flag)); // Output: false
+        }
+    }
+    
+    
+    
+
