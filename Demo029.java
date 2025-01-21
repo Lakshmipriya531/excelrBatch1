169f694f8ae6062aba7b16f55ff4c7bc345d08b7
Lakshmipriya531 <priyapasupuleti561@gmail.com>
2025-01-21 10:52:12 +0530

    Add files via upload

---
+++ Demo029.java
@@ -0,0 +1,13 @@
+public class Demo029 {
+        public static void main(String[] args) {
+            String[] friends = {"Srividya", "Swapna", "Bhavya", "Akhila", "Sri", "Vidya", "Teja", "Keerthi", "Ian", "Jack"};
+
+            System.out.println("List of Friends:");
+            for (String friend : friends) {
+                System.out.println(friend);
+            }
+        }
+    }
+    
+    
+
