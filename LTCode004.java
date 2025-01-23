661c218ea56cfe29e5ae527b347d84e27cb89a58
Lakshmipriya531 <priyapasupuleti561@gmail.com>
2025-01-23 11:19:43 +0530

    Add files via upload

---
+++ LTCode004.java
@@ -0,0 +1,65 @@
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {}
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class LTCode004 {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0); 
+        ListNode current = dummyHead;        
+        int carry = 0; 
+
+        while (l1 != null || l2 != null || carry != 0) {
+            int val1 = (l1 != null) ? l1.val : 0; 
+            int val2 = (l2 != null) ? l2.val : 0; 
+
+            int total = val1 + val2 + carry; 
+            carry = total / 10; 
+            int newDigit = total % 10; 
+
+            current.next = new ListNode(newDigit); 
+            current = current.next; 
+
+            if (l1 != null) l1 = l1.next; 
+            if (l2 != null) l2 = l2.next; 
+        }
+
+        return dummyHead.next; 
+    }
+
+    
+    public static void printList(ListNode node) {
+        while (node != null) {
+            System.out.print(node.val);
+            if (node.next != null) {
+                System.out.print(" -> ");
+            }
+            node = node.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        
+        ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+        ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4))); 
+
+        LTCode004 solution = new LTCode004();
+        ListNode result = solution.addTwoNumbers(l1, l2);
+
+        System.out.print("Result: ");
+        printList(result); 
+    }
+}
